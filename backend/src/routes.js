26af4d2a90f3d9d054a9a0c319834ff6464f4c36
jptrindade3 <jpr.trindade@gmail.com>
2020-05-12 19:34:43 -0300

    Schema products
    Foi criado o Schema dos products e rotas de cadastro e visualização desse schema para seu teste

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,46 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
 const routes = express.Router();
 
+
 routes.get('/', (req,res)=> {
-    return res.json('Joao é lindo');
+    return res.json('Souzão é lindo');
+});
+
+routes.get('/productList', async(req,res) => {
+    const products = await connection('product').select('*');
+
+    return res.json(products);
+});
+
+routes.post('/product', async(req,res) => {
+    const { 
+    name, 
+    shelfLife, 
+    location, 
+    type, 
+    lastBuyDate, 
+    lastBuyPrice, 
+    amount, 
+    allertAmount} = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('product').insert({
+        id,
+        name, 
+        shelfLife, 
+        location, 
+        type, 
+        lastBuyDate, 
+        lastBuyPrice, 
+        amount, 
+        allertAmount
+    });
+
+    return res.json({ id });
 });
 
 module.exports = routes;
