67d049836d8e0e721cafa511a24d69a8032910ec
Izabela Costa Brant <izabelabrant@cpejr.com.br>
2020-05-18 21:03:35 -0300

    Função onClick do botão e onChange do input - página de Login.

--- frontend/src/Pages/Login/login.js
+++ frontend/src/Pages/Login/login.js
@@ -1,67 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './loginStyle';
 import { Typography } from '@material-ui/core';
 
 
-export default function Login(){
+export default function Login() {
 
-   /* return(
-        <div style={styles.container}>
-           <div style={styles.logo}>
-               <img alt="Logo" src='./Logo.png' style={{height: "80%", width: 'auto'}}/>
-           </div>
+    /* return(
+         <div style={styles.container}>
+            <div style={styles.logo}>
+                <img alt="Logo" src='./Logo.png' style={{height: "80%", width: 'auto'}}/>
+            </div>
+             <div style={styles.typoDiv}>
+             <Typography style={styles.typoStyle}>
+                 USUÁRIO
+             </Typography>
+             </div>
+            <input
+                type='text'
+                //placeholder='USUÁRIO'
+                style={styles.inputField}
+
+            />
             <div style={styles.typoDiv}>
             <Typography style={styles.typoStyle}>
-                USUÁRIO
+                SENHA
             </Typography>
             </div>
-           <input
-               type='text'
-               //placeholder='USUÁRIO'
-               style={styles.inputField}
 
-           />
-           <div style={styles.typoDiv}>
-           <Typography style={styles.typoStyle}>
-               SENHA
-           </Typography>
-           </div>
+            <input
+                type='password'
+                //placeholder='SENHA'
+                style={styles.inputField}
 
-           <input
-               type='password'
-               //placeholder='SENHA'
-               style={styles.inputField}
+            />
 
-           />
+            <button style={styles.buttonLogin}>LOGIN</button>
+     )
 
-           <button style={styles.buttonLogin}>LOGIN</button>
-    )
+ }*/
+    const [userName, setUserName] = useState();
+    const [userPassword, setPassword] = useState('Adm');
+    function handleSubmit() {
+        alert(`Usuário ${userName} logado com sucesso com a senha ${userPassword}`)
+    }
 
-}*/
-   return(
-       <div style={styles.container}>
-           <div style={styles.logo}>
-               <img alt="Logo" src='./Logo.png' style={{height: "80%", width: 'auto'}}/>
-           </div>
+        return (
+            <div style={styles.container}>
+                <div style={styles.logo}>
+                    <img alt="Logo" src='./Logo.png' style={{height: "80%", width: 'auto'}}/>
+                </div>
 
-           <input
-               type='text'
-               placeholder='USUÁRIO'
-               style={styles.inputField}
+                <input
+                    type='text'
+                    placeholder='USUÁRIO'
+                    style={styles.inputField}
+                    onChange={(e)=>{setUserName(e.target.value)}}
 
-           />
+                />
 
-           <input
-               type='password'
-               placeholder='SENHA'
-               style={styles.inputField}
+                <input
+                    type='password'
+                    placeholder='SENHA'
+                    style={styles.inputField}
+                    onChange={(e)=>{setPassword(e.target.value)}}
+                />
 
-           />
-
-           <button style={styles.buttonLogin}>LOGIN</button>
+                <button style={styles.buttonLogin} onClick={handleSubmit}>LOGIN</button>
 
 
-       </div>
+            </div>
 
-   )
+        )
+
 }
