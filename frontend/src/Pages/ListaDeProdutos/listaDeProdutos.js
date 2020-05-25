e5099a301adefa3e31ed3b7e231f0af30a20ba63
Izabela Costa Brant <izabelabrant@cpejr.com.br>
2020-05-25 19:19:29 -0300

    Página de Lista de Produtos - incompleta.

--- frontend/src/Pages/ListaDeProdutos/listaDeProdutos.js
+++ frontend/src/Pages/ListaDeProdutos/listaDeProdutos.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import styles from './listaDeProdutosStyle';
+import {Typography} from "@material-ui/core";
 
 export default function ListaDeProdutos(){
 
     return(
-        <div style={styles.titulo}>
-            Lista de produtos
+        <div style={styles.container}>
+           <div style={styles.searchContainer}>
+               <div style={styles.fieldContainer}>
+                   <Typography style={styles.typoStyle}>
+                       PRODUTO
+                   </Typography>
+                   <input
+                       style={styles.inputField}
+                       type='text'
+                   />
+               </div>
+               <div style={styles.fieldContainer}>
+                   <Typography style={styles.typoStyle}>
+                       TIPO
+                   </Typography>
+                   <input
+                       style={styles.inputField}
+                       type='text'
+                   />
+               </div>
+                <button style={styles.buttonSearch}>BUSCAR</button>
+
+           </div>
+           <div style={styles.listContainer}>
+                Listar
+           </div>
         </div>)
 
 }
--- frontend/src/Pages/ListaDeProdutos/listaDeProdutosStyle.js
+++ frontend/src/Pages/ListaDeProdutos/listaDeProdutosStyle.js
@@ -1,7 +1,70 @@
+import {azulPadrao, fontCinzaEscuro, brancoPadrao, fontPadrao, fontSizeButton, fontSizeTitle} from '../../StylePadrao/stylePadrao'
+
 const styles = {
-    titulo: {
-       marginLeft: '20rem',
-        fontSize: '1.5rem',
-    }
+    container: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    },
+    searchContainer:{
+      height: '20%',
+      width: '80%',
+      backgroundColor: brancoPadrao,
+      display: 'flex',
+      flexDirection: 'row'
+
+    },
+    fieldContainer:{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '18%',
+        margin: '0% 0% 0% 5%'
+    },
+    typoStyle:{
+        color: fontCinzaEscuro,
+        fontFamily: fontPadrao,
+        fontSize: '20px',
+        alignItems:"left",
+        marginBottom: '1%'
+    },
+    inputField: {
+        borderRadius: '50px',
+        // marginLeft: '10%',
+        marginBottom: '2%',
+        border: '1px solid #707070',
+        backgroundColor: '#D3D3D3',
+        height: '35%',
+        width: '100%',
+        fontSize: '120%',
+        fontFamily: fontPadrao,
+        color: fontCinzaEscuro,
+        paddingLeft: '5%',
+        alignItems: 'center'
+    },
+    buttonSearch: {
+        borderRadius: '50px',
+        margin: '5%',
+        backgroundColor: azulPadrao,
+        height: '35%',
+        width: '18%',
+        fontSize: fontSizeButton,
+        fontFamily: fontPadrao,
+        color: brancoPadrao,
+        alignItems: 'center',
+    },
+
+
+    listContainer:{
+        height: '50%',
+        width: '80%',
+        backgroundColor: brancoPadrao,
+        marginTop: '5%'
+    },
 };
 export default styles;
