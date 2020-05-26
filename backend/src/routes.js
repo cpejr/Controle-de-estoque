a530002b64657875cc2582dbbd32657e868f273a
Raphael Lima <raphaellima@cpejr.com.br>
2020-05-26 19:39:39 -0300

    Criação de rotas delete, create e specificUser
    Nessas rotas a aplicação poderá criar usuários e deletar  e seleconar um em específico, porém só a rota create não está com permissão

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,13 +6,28 @@ const routes = express.Router();
 
 
 routes.get('/', (req,res)=> {
-    return res.json('Souzão é lindo');
+    return res.json('joão é lindo');
 });
 
 routes.get('/productList', productController.index);
 routes.post('/newProduct', productController.create);
 
+//Rotas de usuário:
+/*
+    Utilizamos metodos do arquivo userController.js para criar todas as funções das rotas.
+*/
+
+//Rota para criar um novo usuário
 routes.post('/newUser', userController.create);
+
+//Rota para listar todos os usuários do sistema
 routes.get('/Listuser', userController.index);
 
+//Rota para deletar um usuário específico do sistema
+routes.delete('/deleteUser/:userCPF', userController.delete);
+
+//Rota para selecionar um usuário específico
+routes.get('/selectUser/:userCPF', userController.selectUser);
+
 module.exports = routes;
+
