19c622c67dfab2a41ff2b5ebe0cfe9b20c3fdd70
Carolina <carolinaalvares@cpejr.com.br>
2020-06-14 22:42:35 -0300

    tentando puxar dados das outras tabelas
    no storagechangecontroller. muitas vezes tive que deletar a db e dar migrate latest, run seed e ate msm deletar e criar de novo a tabela. ai depois da certo geralmente. ainda nao conferi os wheres e os inserts.

--- backend/src/controllers/storageChangeRecordController.js
+++ backend/src/controllers/storageChangeRecordController.js
@@ -1,48 +1,53 @@
 const connection = require('../database/connection');
+const { from } = require('../database/connection');
 
 module.exports = {
     //o que será criado e editado por nós. Especificos dessa edicao. 
     async create(req, res){
+        
         const {
-            amount_after,
+            user_name,
+            amount_after
         } = req.body;
     
     //usaremos o id do produto para a autorizacao e para determinar o contexto da requisicao
         const product_id =req.headers.authorization;
+
         const date = new Date()
         const updated_at =  date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+'_'+date.getHours()+':'+date.getMinutes();
-        const product_name = "carolinas";
-        const product_amount=10;
-        const user_name = "natalia";
+
+        const product_name = knex.from('product').where({id, product_id}).select({product_id}).insert({id});
+        const product_amount= knex.from('product').where({id, product_id}).select({amount_before}).insert({amount});
+
     //inserir nas colunas da table. Completar com resto
         const [change] = await connection('storageChangeRecord').insert({
             amount_after,
             updated_at,
             product_id,
             product_name,
-            product_amount,
+            amount_before,
             user_name
         });
         return res.json({ change, updated_at });
     },
 
 
-    async index(req,res){
+//     async index(req,res){
         
-        // import knex from '../database/connection';
-        // import express from 'express';
 
-        const product= await knex('product').select('*');
+//         // import express from 'express';
 
-        const serialized= product.map(product => {
-        return {
-            product_id: product.id,
-            product_name: product.name,
-            product_amount: product.amount,
-        };
-        });
-        return response.json(serialized);
-    },
+//         const product= await knex('product').select('*');
+
+//         const serialized= product.map(product => {
+//         return {
+//             product_id: product.id,
+//             product_name: product.name,
+//             product_amount: product.amount,
+//         };
+//         });
+//         return response.json(serialized);
+//     },
 
 
 };
