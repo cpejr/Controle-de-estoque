680c94fdc45e95961e2484f52dc8ee0d32838c6a
Elias Faria <eliasfaria@ufmg.br>
2020-06-23 09:44:22 -0300

    avaregeConsuption e Editproduct

--- backend/src/controllers/storageChangeRecordController.js
+++ backend/src/controllers/storageChangeRecordController.js
@@ -2,72 +2,118 @@ const connection = require('../database/connection');
 
 module.exports = {
     //o que será criado e editado por nós. Especificos dessa edicao. 
-    async create(req, res){
-        
+    async create(req, res) {
+
         const {
             product_id,
-            changed
+            changed,
+            updated_at
         } = req.body;
-    
-    //usaremos o id do produto para a autorizacao e para determinar o contexto da requisicao
+
+        //usaremos o id do produto para a autorizacao e para determinar o contexto da requisicao
         const user_name = req.headers.name;
 
         const date = new Date()
-        const updated_at =  date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+'_'+date.getHours()+':'+date.getMinutes();
-        
-        try{
-             const product_name = await connection('product')
-        .where('id', product_id)
-        .select('name')
-        .first();
-       
-        
-        const name = product_name.name; 
+        console.log(date);
+        //const updated_at = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 
-        const amount = await connection('product')
-        .where('id', product_id)
-        .select('amount')
-        .first();
-        
-        const newAmount = amount.amount + changed;
+        try {
+            const product_name = await connection('product')
+                .where('id', product_id)
+                .select('name')
+                .first();
 
-        if(newAmount>= 0 ){
 
-            await connection('storageChangeRecord').insert({
-                product_name: name,
-                product_id,
-                changed,
-                updated_at,
-                user_name,
-                newAmount
-            })
+            const name = product_name.name;
 
-            await connection('product')
-            .where('id',product_id)
-            .update({
-                amount: newAmount
-            })
+            const amount = await connection('product')
+                .where('id', product_id)
+                .select('amount')
+                .first();
 
-        
-        return res.json({ updated_at, name, newAmount, user_name, product_id });
-        }   
+            const newAmount = amount.amount + changed;
 
-        else{
-            return res.json("ERRO numero");
+            if (newAmount >= 0) {
+
+                await connection('storageChangeRecord').insert({
+                    product_name: name,
+                    product_id,
+                    changed,
+                    updated_at,
+                    user_name,
+                    newAmount
+                })
+
+                await connection('product')
+                    .where('id', product_id)
+                    .update({
+                        amount: newAmount
+                    })
+
+
+                return res.json({ updated_at, name, newAmount, user_name, product_id });
+            }
+
+            else {
+                return res.json("ERRO numero");
+            }
         }
-    }
-        
-    catch(ERRO){
-        return res.json("ERRO id");
-    }
-    
+
+        catch (ERRO) {
+            return res.json("ERRO id");
+        }
+
     },
 
-    // Metodo para listar todos os usuários do sistema 
-    async index(req,res){
+    // Metodo para listar todos as alterações no estoque
+    async index(req, res) {
         const history = await connection('storageChangeRecord').select('*');
 
         return res.json(history);
+    },
+
+    async averageConsumption(req, res) {
+        const {
+            product_id,
+            date
+        } = req.body;
+        console.log(
+            product_id,
+            date
+        );
+
+        const info = await connection('StorageChangeRecord')
+            .where('product_id', product_id)
+            .andWhere('changed', '<', 0)
+            .andWhere('updated_at', '>=', date)
+            .select('product_name','changed','updated_at');
+
+        const sum = await connection('StorageChangeRecord')
+            .sum('changed')
+            .where('product_id', product_id)
+            .andWhere('changed', '<', 0)
+            .andWhere('updated_at', '>=', date);
+      
+        console.log(sum);
+
+        if (sum != null) {
+            return res.status(200).json(info);
+        }
+        else {
+            return res.status(401).json({ error: "Operação invalida" });
+        }
+
+    },
+
+    async delete(req, res){
+        const {
+            id,
+        } = req.body;
+        // const userType = request.headers.authorization;
+
+        await connection('StorageChangeRecord').where('product_id', id).delete();
+
+        return res.status(204).send();//Status de sucesso
     },
 
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,13 +10,14 @@ routes.get('/', (req,res)=> {
     return res.json('Souzão é lindo');
 });
 
+//PRODUCTS ROUTES
 routes.get('/productManagement', productController.index);
 routes.post('/productManagement', productController.create);
 routes.delete('/productManagement/:id', productController.delete);
 routes.get('/search/:id', productController.search);
+routes.put('/editProduct', productController.editProduct);
 
-
-
+//USER ROUTES
 routes.post('/newUser', userController.create);
 routes.get('/listUser', userController.index);
 routes.delete('/deleteUser/:userCPF', userController.delete);
@@ -24,17 +25,17 @@ routes.get('/selectUser/:userCPF', userController.selectUser);
 routes.put('/edituserpass', userController.editpass);
 routes.put('/editinfo', userController.edituserinfo);
 
+//STORAGE RECORD ROUTES
 routes.get('/storageChangeRecord', storageChangeRecordController.index);
 routes.post('/storageChangeRecord', storageChangeRecordController.create);
-// routes.get('/averageConsumption/:product_id',storageChangeRecordController.averageConsumption)
-
+routes.get('/averageConsumption',storageChangeRecordController.averageConsumption);
+routes.delete('/averageConsumptiondelete',storageChangeRecordController.delete);
 
 
 routes.delete('/productList/:productID', productListController.delete);
 routes.get('/productListSearch/:productID', productListController.search);
 routes.get('/productList', productListController.index);
 routes.post('/productList', productListController.create);
-
 
 
 
